6bcd64fccc93150f4ecfd2e6de51845355256d9f
Ion Drimba Filho <iondrimba@gmail.com>
2020-03-10 01:00:54 -0300

    wip

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -14,16 +14,19 @@ const distance = (x1, y1, x2, y2) => {
 
 export default class App {
   constructor() {
-    this.tiles = [];
-    this.cols = 2;
-    this.rows = 2;
+    this.cols = 6;
+    this.rows = 4;
+    this.coneRadius = .6;
+    this.coneHeight = 3.6;
+    this.coneSpacing = 1.8;
+    this.cones = [];
 
     this.angle = 0;
     this.amplitude = 0;
     this.frequency = 1;
     this.wavelength = 0;
     this.speed = 1000;
-
+    this.flipped = false;
     this.init();
 
     window.addEventListener('resize', this.onResize.bind(this), { passive: true });
@@ -32,7 +35,6 @@ export default class App {
   init() {
     this.floorShape = this.createShape();
     this.secondfloorShape = this.createShape();
-    // this.scene.add(this.secondfloorShape);
 
     this.createScene();
     this.createCamera();
@@ -43,40 +45,39 @@ export default class App {
     this.ground = this.addGround(this.floorShape);
     this.scene.add(this.ground);
 
-    // this.ground.rotation.y = radians(-45);    
-    // this.groupCones.rotation.z = radians(-45);
-
-    // INVERT CAMERA FOR SECOND VIEW
-    // ALSO CREATE A SECOND DIRECTIONAL LIGHT
-    // this.camera.position.set(30, -30, -30);
-
+    this.groupCones.position.set(-5, 0, 5);    
+    this.ground.position.set(-5, 0, 5);
+    this.groupCones.rotation.y = radians(-90);
+    this.ground.rotation.z = radians(90);
+    
     this.animate();
+
+    this.animateGrid();
   }
 
   addDirectionalLight() {
-    const light = new THREE.DirectionalLight(0xffffff, 1);
-    light.position.set(0, 1, 0);
-    light.castShadow = true;
-    
-    light.shadow.camera.far = 2000;
-    light.shadow.camera.near = -50;
+    this.directionalLight = new THREE.DirectionalLight(0xffffff, 1, 10);
+    this.directionalLight.castShadow = true;
+    this.directionalLight.position.set(0, 1, 0);
 
-    light.shadow.camera.left = -20;
-    light.shadow.camera.right = 20;  
-    light.shadow.camera.top = 15;  
-    light.shadow.camera.bottom = -15;  
-    light.shadow.camera.zoom = 1;
+    this.directionalLight.shadow.camera.far = 1000;
+    this.directionalLight.shadow.camera.near = -100;
+
+    this.directionalLight.shadow.camera.left = -20;
+    this.directionalLight.shadow.camera.right = 20;
+    this.directionalLight.shadow.camera.top = 15;
+    this.directionalLight.shadow.camera.bottom = -15;
+    this.directionalLight.shadow.camera.zoom = 1;
 
     const targetObject = new THREE.Object3D();
     targetObject.position.set(-20, -5, -10);
+    this.directionalLight.target = targetObject;
 
-    light.target = targetObject;
+    this.scene.add(this.directionalLight);
+    this.scene.add(this.directionalLight.target);
 
-    this.scene.add(light);
-    this.scene.add(light.target);
-
-    var helper = new THREE.CameraHelper( light.shadow.camera );
-    this.scene.add( helper );
+    // var helper = new THREE.CameraHelper(this.directionalLight.shadow.camera);
+    // this.scene.add(helper);
   }
 
   createScene() {
@@ -93,20 +94,34 @@ export default class App {
   createCamera() {
     const fov = 25;
     const aspect = window.innerWidth / window.innerHeight;
-    const near = 1; 
+    const near = 1;
     const far = 1000;
     this.camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
     this.camera.position.set(-30, 30, 30);
     this.scene.add(this.camera);
   }
 
+  flipCamera() {
+    this.flipped = !this.flipped;
+
+    if (this.flipped) {
+      this.scene.rotation.x = radians(-180);
+      this.camera.lookAt(this.scene.position);
+      this.directionalLight.target.position.set(10, -5, 20);
+    } else {
+      this.directionalLight.target.position.set(-20, -5, -10);
+      this.scene.rotation.x = radians(0);
+      this.camera.lookAt(this.scene.position);
+    }
+  }
+
   addCameraControls() {
     this.controls = new THREE.OrbitControls(this.camera, this.renderer.domElement);
-    this.controls.maxPolarAngle = radians(50);
+    // this.controls.maxPolarAngle = radians(50);
   }
 
   addAmbientLight() {
-    const light = new THREE.AmbientLight('#fed244', 1, 100);
+    const light = new THREE.AmbientLight('#fed244', 1, 1000);
     this.scene.add(light);
   }
 
@@ -126,36 +141,33 @@ export default class App {
   }
 
   addCones(shape) {
-    const spacing = 2;
-    const geometry = new THREE.CylinderGeometry(1, 1, 4, 64);
+    const geometry = new THREE.CylinderGeometry(this.coneRadius, this.coneRadius, this.coneHeight, 32);
     const material = new THREE.MeshLambertMaterial({ color: '#fed244' });
-    const finalPos = (i) => { return (-i * spacing); }
+    const finalPos = (i) => { return (-i * this.coneSpacing); }
 
     this.groupCones = new THREE.Object3D();
 
     for (let i = 0; i < this.cols; i++) {
-      this.tiles[i] = [];
-
       for (let j = 0; j < this.rows; j++) {
         const x = finalPos(i);
         const y = finalPos(j);
 
         this.createHole(shape, x, y);
 
-        this.tiles[i][j] = this.createCone(geometry, material);
-        this.tiles[i][j].position.set(x - (i * spacing), 0, y - (j * spacing));
+        const cone = this.createCone(geometry, material);
+        cone.position.set(x - (i * this.coneSpacing), -(this.coneHeight * .5), y - (j * this.coneSpacing));
 
-        this.groupCones.add(this.tiles[i][j]);
+        this.cones.push(cone);
+        this.groupCones.add(cone);
       }
     }
 
-    // this.groupCones.position.set(5, 0, 5);
     this.scene.add(this.groupCones);
   }
 
   createHole(shape, x, y) {
     const holePath = new THREE.Path();
-    const radius = 1;
+    const radius = this.coneRadius;
 
     holePath.moveTo(x, y);
     holePath.ellipse(x, y, radius, radius, 0, Math.PI * 2);
@@ -183,8 +195,6 @@ export default class App {
 
     mesh.rotation.set(Math.PI * 0.5, 0, 0);
     mesh.material.needsUpdate = true;
-
-    // mesh.position.set(5, 0, 5);
     mesh.receiveShadow = true;
 
     return mesh;
@@ -193,35 +203,79 @@ export default class App {
   createCone(geometry, material) {
     const obj = new THREE.Mesh(geometry, material);
     obj.position.y = 0;
-    obj.receiveShadow = true;
-    obj.castShadow = true;
 
-    const pivot = new THREE.Object3D();
-    pivot.add(obj);
-    pivot.size = 0;
-
-    return pivot;
+    return obj;
   }
 
-  drawWave() {
-    for (let i = 0; i < this.cols; i++) {
-      for (let j = 0; j < this.rows; j++) {
-        const dist = distance(j, i, -this.rows, -this.cols);
-        const offset = map(dist, -this.wavelength, 100, -140, this.wavelength);
-        const angle = this.angle + offset;
-        const y = map(Math.sin(angle), -1, 1, -4, this.amplitude);
-
-        this.tiles[i][j].position.y = y;
+  animateIn(element, index, onComplete = () => {}) {
+    TweenMax.to(element.position, .3, {
+      y: (this.coneHeight * .5),
+      ease: Sine.easeOut,
+      delay: index * .02,
+      onUpdate: () => {
+        element.castShadow = true;
+      },
+      onCompleteParams: [element, index],
+      onComplete: (elm, i) => { 
+        onComplete(elm, i);
       }
+    })
+  }
+
+  animateOut(element, index) {
+    TweenMax.to(element.position, 1.5, {
+      y: -(this.coneHeight * .5),
+      ease: Elastic.easeOut.config(1, .3),
+      onCompleteParams: [element, index],
+      onComplete: (elm, i) => {
+        elm.castShadow = false;
+
+        if (i===this.cones.length-1) {
+          this.flipCamera();
+          this.animateGrid();
+        }
+      }
+    })
+  }
+
+  animateGrid() {
+    for (let index = 0; index < this.cones.length; index++) {
+      const element = this.cones[index];
+
+      this.animateIn(element, index, this.animateOut.bind(this));
     }
 
-    this.angle -= this.frequency / 20;
+    // gsap.to(this.cones, 0, {
+    //   stagger: (index, target) => {
+    //     TweenMax.to(target.position, 2, { 
+    //       y: 2, 
+    //       ease: Elastic.easeOut.config(1, .5), 
+    //       delay: index * .02,
+    //       yoyo: true, 
+    //       yoyoEase: Elastic.easeOut.config(1.5, .6), 
+    //       repeat: 1,
+    //       onUpdate: () => {
+    //         target.children[0].receiveShadow = true;
+    //         target.children[0].castShadow = true;
+    //       },
+    //       onCompleteParams: [target, index], 
+    //       onComplete: (elm, i) => {
+    //         elm.children[0].receiveShadow = false;
+    //         elm.children[0].castShadow = false;
+
+    //         if(i===this.cones.length-1) {
+    //           this.flipCamera();
+    //           this.animateGrid();
+    //         }
+    //       }
+    //     })
+    //   },
+    // });
   }
 
   animate() {
     this.controls.update();
     this.renderer.render(this.scene, this.camera);
-    this.drawWave();
     requestAnimationFrame(this.animate.bind(this));
   }
 
